194ab456c71e27ad561cd490dcecdfab907359ee
Jarek Machaj <jarekmachaj@users.noreply.github.com>
2018-03-29 20:38:26 +0200

    pierwsza lekcja

---
+++ javascript/HelloWorld.js
@@ -0,0 +1,9 @@
+function mojProgram(){
+	var imie = "";
+	var wiek = 0;
+	var imie = prompt("Podaj imie");
+    var wiek = prompt('Podaj wiek');
+    var msg = "imie: " + imie + ", wiek: " + wiek;
+    alert(msg);
+    console.log(msg);
+}
---
+++ javascript/simpleNodeJS.server.js
@@ -0,0 +1,18 @@
+const http = require('http')
+const port = 5000
+
+const requestHandler = (request, response) => {
+  console.log(request.connection.remoteAddress);
+  console.log(request.url)  
+  response.end('Hola!')
+}
+
+const server = http.createServer(requestHandler)
+
+server.listen(port, (err) => {
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+
+  console.log(`server is listening on ${port}`)
+})
